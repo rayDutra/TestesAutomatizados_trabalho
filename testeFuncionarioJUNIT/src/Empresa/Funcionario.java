e4cc527cb035e7d9b20a919449165164b0144966
rayDutra <rayanneoliveiradutra@gmail.com>
2023-04-13 21:57:45 -0300

    Segundo Commit

--- testeFuncionarioJUNIT/src/Empresa/Funcionario.java
+++ testeFuncionarioJUNIT/src/Empresa/Funcionario.java
@@ -1,6 +1,64 @@
 package Empresa;
 
 public class Funcionario {
-	
+	private String nome;
+	private int horasTrabalhadas;
+	private double valorHora;
+
+	public Funcionario(String nome, int horasTrabalhadas, double valorHora) {
+		this.nome = nome;
+		this.horasTrabalhadas = validaHorasTrabalhadas(horasTrabalhadas);
+		this.valorHora = validaValorHorasTrabalhadas(valorHora);
+	}
+
+	public double calcularPagamento() {
+		double pagamento = valorHora * horasTrabalhadas;
+		if (pagamento < 1320.00) {
+			pagamento = 1320.00;
+		}
+		return pagamento;
+	}
+
+	// Getters e Setters
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getHorasTrabalhadas() {
+		return horasTrabalhadas;
+	}
+
+	public void setHorasTrabalhadas(int horasTrabalhadas) {
+		this.horasTrabalhadas = validaHorasTrabalhadas(horasTrabalhadas);
+	}
+
+	public double getValorHora() {
+		return valorHora;
+	}
+
+	public void setValorHora(double valorHora) {
+		this.valorHora = validaValorHorasTrabalhadas(valorHora);
+	}
+	private int validaHorasTrabalhadas(int horasTrabalhadas) {
+		if (horasTrabalhadas > 40) {
+			throw new IllegalArgumentException(
+					"O número de horas trabalhadas por funcionários próprios deve ser menor ou igual a 40.");
+		}
+		return horasTrabalhadas;
+	}
+	private double validaValorHorasTrabalhadas(double valorHora) throws IllegalArgumentException{
+		double salarioMinimo= 1320.00;
+		double valorMininimoHora = salarioMinimo * 0.04;
+		double valorMaximoHora = salarioMinimo * 0.10;
+		if(valorHora < valorMininimoHora | valorHora < valorMaximoHora) {
+			throw new IllegalArgumentException(
+					"O valor por hora precisa estr enre 4% e 10% do salário minimo.");
+		}
+		return valorHora;
+	}
 	
 }
--- testeFuncionarioJUNIT/src/Empresa/FuncionarioTerceirizado.java
+++ testeFuncionarioJUNIT/src/Empresa/FuncionarioTerceirizado.java
@@ -1,5 +1,30 @@
 package Empresa;
 
 public class FuncionarioTerceirizado extends Funcionario {
+    private double despesasAdicionais;
 
+    public FuncionarioTerceirizado(String nome, int horasTrabalhadas, double valorHora, double despesasAdicionais) {
+        super(nome, horasTrabalhadas, valorHora);
+        this.despesasAdicionais = despesasAdicionais;
+    }
+
+    @Override
+    public double calcularPagamento() {
+        double pagamento = super.calcularPagamento() + (despesasAdicionais * 1.10);
+        if (pagamento < 1320.00) {
+            pagamento = 1320.00;
+        }
+        return pagamento;
+    }
+    public double getDespesasAdicionais() {
+        return despesasAdicionais;
+    }
+
+    public void setDespesasAdicionais(double despesasAdicionais) {
+        if (despesasAdicionais > 1000.00) {
+            this.despesasAdicionais = 1000.00;
+        } else {
+            this.despesasAdicionais = despesasAdicionais;
+        }
+    }
 }
